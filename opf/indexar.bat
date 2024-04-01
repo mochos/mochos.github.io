ccad3be3195a85dc195199300c98bedacff352d5
lorspi <juan.perez@lorspi.com>
2024-03-31 23:15:05 -0500

    Indices
    - Corregido indice de opf
    - Creado indice de skins
    - Creado visualizador de skins

--- opf/indexar.bat
+++ opf/indexar.bat
@@ -8,7 +8,7 @@ echo. > "%output_file%"
 
 for /r %%i in (*.jpg *.jpeg *.png *.gif *.bmp *.tif *.tiff) do (
     set "file_path=%%i"
-    set "file_path=!file_path:D:\Recursos\xampp-htdocs\=https://mochos.xyz/!"
+    set "file_path=!file_path:D:\GitHub\mochos.github.io\opf\=!"
     set "file_path=!file_path:\=/!"
     echo ^<a href=!file_path! target=_blank^>!file_path!^</a^>^<br^> >> "%output_file%"
 )
---
+++ skins/indexar.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "output_file=index.html"
+
+echo Listing image files in current directory and subdirectories...
+echo. > "%output_file%"
+
+for /r skins %%i in (*.jpg *.jpeg *.png *.gif *.bmp *.tif *.tiff) do (
+    set "file_path=%%i"
+    set "file_path=!file_path:D:\GitHub\mochos.github.io\skins\skins\=!"
+    set "file_path=!file_path:.png=!"
+    set "file_path=!file_path:\=/!"
+    echo ^<a href=skin.html?nombre=!file_path!^>!file_path!^</a^>^<br^> >> "%output_file%"
+)
+
+echo List created: %output_file%
